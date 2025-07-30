f673b0ac8c8161c1b56c5852cafe8fc7e34bb103
Yemi <yajibola23gmail.com>
2025-07-30 15:25:29 -0500

    🧹 removed Info.plist from git tracking

--- FitPulseDemo/ContentView.swift
+++ FitPulseDemo/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    let api = API()
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ FitPulseDemo/HeartRateMonitorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  HeartRateMonitorViewModel.swift
+//  FitPulseDemo
+//
+//  Created by Amira Ajibola  on 7/30/25.
+//
+
+import Foundation
+import SmartSpectraSwiftSDK
+
+
