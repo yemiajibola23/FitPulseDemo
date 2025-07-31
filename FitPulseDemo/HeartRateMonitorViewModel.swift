081b46ed56f5517a8fdd518ac1204de1939e56be
Yemi <yajibola23gmail.com>
2025-07-31 17:45:46 -0500

    ✨ feat: Added HeartRateMontiorViewModdel to setup SDK and calculate min, max and average pulse.

--- FitPulseDemo/HeartRateMonitorViewModel.swift
+++ FitPulseDemo/HeartRateMonitorViewModel.swift
@@ -7,5 +7,69 @@
 
 import Foundation
 import SmartSpectraSwiftSDK
+import Combine
+import SwiftUI
+
+@Observable
+class HeartRateMonitorViewModel {
+    var bpm: Int?
+    var minBPM = Int.max
+    var maxBPM = Int.min
+    var avgBPM = 0
+    var errorMessage: String? = nil
+    
+    private var history: [Int] = []
+    private var cancellable: AnyCancellable?
+    
+    func configureSDK() {
+        let sdk = SmartSpectraSwiftSDK.shared
+        
+        sdk.setApiKey(APIKeyManager.presageAPIKey)
+        
+        sdk.setHeadlessMode(true)
+        
+        sdk.setSmartSpectraMode(.continuous)
+        
+        sdk.setMeasurementDuration(1.0)
+        sdk.setRecordingDelay(0)
+        
+        sdk.setCameraPosition(.back)
+        
+        sdk.showControlsInScreeningView(false)
+        
+        sdk.setShowFps(false)
+        
+        SmartSpectraVitalsProcessor.shared.startProcessing()
+    }
+    
+    func startMonitoring() {
+        configureSDK()
+        
+        cancellable = SmartSpectraSwiftSDK.shared
+            .$metricsBuffer
+            .compactMap { $0?.pulse.rate.last?.value }
+            .sink { [weak self] newValue in
+                self?.updateStats(with: Int(newValue))
+            }
+    }
+    
+    private func updateStats(with newBPM: Int) {
+        bpm = newBPM
+        history.append(newBPM)
+        minBPM = min(newBPM, minBPM)
+        maxBPM = max(newBPM, maxBPM)
+        avgBPM = history.reduce(0, +) / history.count
+    }
+    
+    func stopMonitoring() {
+        cancellable?.cancel()
+        history.removeAll()
+        
+        bpm = nil
+        minBPM = Int.max
+        maxBPM = Int.min
+        avgBPM = 0
+    }
+}
 
 
